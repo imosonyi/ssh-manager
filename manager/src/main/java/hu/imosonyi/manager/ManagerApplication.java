14e8695c22e8b57405932facf97f8700f59319f9
imosonyi <mosonyi15p@yahoo.com>
2021-04-08 12:50:09 +0200

    Added empty manager project.

---
+++ manager/src/main/java/hu/imosonyi/manager/ManagerApplication.java
@@ -0,0 +1,13 @@
+package hu.imosonyi.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class, args);
+    }
+
+}
